276eba72b8580670385835e298acbbf5f20c5961
ilgazinho <muhammedilgaz@gmail.com>
2018-10-16 11:13:13 +0300

    Added

---
+++ Form1.Designer.cs
@@ -0,0 +1,422 @@
+﻿namespace perseptron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label18 = new System.Windows.Forms.Label();
+            this.tbb2 = new System.Windows.Forms.TextBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.tbb1 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.tbsonuc = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.tbOgrenme = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tbEsik = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbwx2 = new System.Windows.Forms.TextBox();
+            this.tbwx1 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbx22 = new System.Windows.Forms.TextBox();
+            this.tbx21 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbx12 = new System.Windows.Forms.TextBox();
+            this.tbx11 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.label18);
+            this.panel1.Controls.Add(this.tbb2);
+            this.panel1.Controls.Add(this.label17);
+            this.panel1.Controls.Add(this.tbb1);
+            this.panel1.Controls.Add(this.btnHesapla);
+            this.panel1.Controls.Add(this.tbsonuc);
+            this.panel1.Controls.Add(this.label15);
+            this.panel1.Controls.Add(this.tbOgrenme);
+            this.panel1.Controls.Add(this.label16);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.tbEsik);
+            this.panel1.Controls.Add(this.label14);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.tbwx2);
+            this.panel1.Controls.Add(this.tbwx1);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.tbx22);
+            this.panel1.Controls.Add(this.tbx21);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tbx12);
+            this.panel1.Controls.Add(this.tbx11);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(9, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(742, 423);
+            this.panel1.TabIndex = 0;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(286, 59);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(20, 13);
+            this.label18.TabIndex = 30;
+            this.label18.Text = "B2";
+            // 
+            // tbb2
+            // 
+            this.tbb2.Location = new System.Drawing.Point(284, 75);
+            this.tbb2.Name = "tbb2";
+            this.tbb2.Size = new System.Drawing.Size(28, 20);
+            this.tbb2.TabIndex = 6;
+            this.tbb2.Text = "0";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(283, 7);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(20, 13);
+            this.label17.TabIndex = 28;
+            this.label17.Text = "B1";
+            // 
+            // tbb1
+            // 
+            this.tbb1.Location = new System.Drawing.Point(284, 24);
+            this.tbb1.Name = "tbb1";
+            this.tbb1.Size = new System.Drawing.Size(28, 20);
+            this.tbb1.TabIndex = 3;
+            this.tbb1.Text = "1";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(194, 266);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(117, 43);
+            this.btnHesapla.TabIndex = 11;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // tbsonuc
+            // 
+            this.tbsonuc.Location = new System.Drawing.Point(318, 3);
+            this.tbsonuc.Multiline = true;
+            this.tbsonuc.Name = "tbsonuc";
+            this.tbsonuc.ReadOnly = true;
+            this.tbsonuc.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbsonuc.Size = new System.Drawing.Size(421, 417);
+            this.tbsonuc.TabIndex = 25;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(204, 209);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(13, 13);
+            this.label15.TabIndex = 24;
+            this.label15.Text = "ʎ";
+            // 
+            // tbOgrenme
+            // 
+            this.tbOgrenme.Location = new System.Drawing.Point(195, 224);
+            this.tbOgrenme.Name = "tbOgrenme";
+            this.tbOgrenme.Size = new System.Drawing.Size(28, 20);
+            this.tbOgrenme.TabIndex = 10;
+            this.tbOgrenme.Text = "0,5";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(20, 220);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(94, 13);
+            this.label16.TabIndex = 22;
+            this.label16.Text = "Öğrenme Katsayısı";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(204, 157);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(13, 13);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "ɸ";
+            // 
+            // tbEsik
+            // 
+            this.tbEsik.Location = new System.Drawing.Point(195, 172);
+            this.tbEsik.Name = "tbEsik";
+            this.tbEsik.Size = new System.Drawing.Size(28, 20);
+            this.tbEsik.TabIndex = 9;
+            this.tbEsik.Text = "-1";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(20, 168);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(61, 13);
+            this.label14.TabIndex = 19;
+            this.label14.Text = "Eşik Değeri";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(241, 106);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(18, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "x2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(204, 106);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "x1";
+            // 
+            // tbwx2
+            // 
+            this.tbwx2.Location = new System.Drawing.Point(243, 123);
+            this.tbwx2.Name = "tbwx2";
+            this.tbwx2.Size = new System.Drawing.Size(28, 20);
+            this.tbwx2.TabIndex = 8;
+            this.tbwx2.Text = "2";
+            // 
+            // tbwx1
+            // 
+            this.tbwx1.Location = new System.Drawing.Point(195, 124);
+            this.tbwx1.Name = "tbwx1";
+            this.tbwx1.Size = new System.Drawing.Size(28, 20);
+            this.tbwx1.TabIndex = 7;
+            this.tbwx1.Text = "1";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(109, 123);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(64, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "W =( x1,x2 )";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(18, 123);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(46, 13);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Ağırlıklar";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(241, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "x2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(207, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "x1";
+            // 
+            // tbx22
+            // 
+            this.tbx22.Location = new System.Drawing.Point(243, 73);
+            this.tbx22.Name = "tbx22";
+            this.tbx22.Size = new System.Drawing.Size(28, 20);
+            this.tbx22.TabIndex = 5;
+            this.tbx22.Text = "1";
+            // 
+            // tbx21
+            // 
+            this.tbx21.Location = new System.Drawing.Point(198, 75);
+            this.tbx21.Name = "tbx21";
+            this.tbx21.Size = new System.Drawing.Size(28, 20);
+            this.tbx21.TabIndex = 4;
+            this.tbx21.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(109, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "X2 =( x1,x2 )";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "2. Örnek";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(241, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "x2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "x1";
+            // 
+            // tbx12
+            // 
+            this.tbx12.Location = new System.Drawing.Point(243, 24);
+            this.tbx12.Name = "tbx12";
+            this.tbx12.Size = new System.Drawing.Size(28, 20);
+            this.tbx12.TabIndex = 2;
+            this.tbx12.Text = "0";
+            // 
+            // tbx11
+            // 
+            this.tbx11.Location = new System.Drawing.Point(195, 24);
+            this.tbx11.Name = "tbx11";
+            this.tbx11.Size = new System.Drawing.Size(28, 20);
+            this.tbx11.TabIndex = 1;
+            this.tbx11.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X1 =( x1,x2 )";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. Örnek";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(754, 442);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Perseptron Örneği";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox tbsonuc;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox tbOgrenme;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox tbEsik;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbwx2;
+        private System.Windows.Forms.TextBox tbwx1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbx22;
+        private System.Windows.Forms.TextBox tbx21;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbx12;
+        private System.Windows.Forms.TextBox tbx11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.TextBox tbb2;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.TextBox tbb1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace perseptron
+{
+    public partial class Form1 : Form
+    {
+        double yeniW1, yeniW2;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Hesaplamalar() {
+            bool hesaplamaIslemiDurumu = true;
+            yeniW1 = Convert.ToDouble(tbwx1.Text);
+            yeniW2 = Convert.ToDouble(tbwx2.Text);
+            int ornek1Sonuc=0,ornek2Sonuc=0;
+            int iterasyon=1;
+            while (hesaplamaIslemiDurumu) 
+            {
+                tbsonuc.Text += "\r\n" + iterasyon.ToString()+".İterasyon "+"1.Örnek" ;
+                ornek1Sonuc = NetHesaplama(
+                                1, iterasyon, Convert.ToDouble(tbx11.Text), Convert.ToDouble(tbx12.Text),
+                                yeniW1, yeniW2,
+                                Convert.ToDouble(tbOgrenme.Text), Convert.ToDouble(tbb1.Text)
+                                );
+                iterasyon++;
+                tbsonuc.Text += "\r\n" + iterasyon.ToString() + ".İterasyon " + "2.Örnek";
+                ornek2Sonuc = NetHesaplama(
+                    2, iterasyon, Convert.ToDouble(tbx21.Text), Convert.ToDouble(tbx22.Text),
+                   yeniW1, yeniW2,
+                   Convert.ToDouble(tbOgrenme.Text), Convert.ToDouble(tbb2.Text)
+                   );
+                iterasyon++;
+                if (ornek1Sonuc == 1 && ornek2Sonuc == 1)
+                {
+                    hesaplamaIslemiDurumu = false;
+                }
+            }          
+            tbsonuc.Text += "\r\n"+"Öğrenme Sonunda ağırlıklar:" +"\r\n" + "w1= "+yeniW1.ToString()+ "\r\n"+"w2= " +yeniW2.ToString();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            tbsonuc.Text = "Perseptron Çözümü" + "\r\n";
+            Hesaplamalar();
+        }
+
+        public int NetHesaplama(int ornekNo,int iterasyon,double x1, double x2, double w1,double w2, double ogrenme,double beklenen) {
+            double net=0;
+            net =  x1* w1 + x2 * w2;
+            tbsonuc.Text+= "\r\n"+"NET= "+net.ToString() ;
+            if (net>Convert.ToDouble(tbEsik.Text))
+            {
+                tbsonuc.Text += "\r\n"+ "Net > ɸ olduğundan Ç" + ornekNo.ToString() + "= 1 olacaktır.";       
+                if (beklenen == 1)
+                {
+                    tbsonuc.Text += "\r\n" + "Ç" + ornekNo.ToString() + "=B" + ornekNo.ToString() +" olduğundan ağırlıklar değişmez."+ "\r\n";
+                    tbsonuc.Text += "W1 ve W2 değerli aynı kalır.\r\nw1=" + w1.ToString() + " w2=" + w2.ToString() + "\r\n";
+                    return 1;
+                    
+                }
+                else
+                {
+                    yeniW1 = w1 - (ogrenme * x1);
+                    tbsonuc.Text += "\r\n" + "W1Yeni= " + w1.ToString() + "-(" + ogrenme.ToString() + "*" + x1.ToString() + ")";
+                    yeniW2 = w2 - (ogrenme * x2);
+                    tbsonuc.Text += "\r\n" + "W2Yeni= " + w2.ToString() + "-(" + ogrenme.ToString() + "*" + x2.ToString() + ")" + "\r\n";
+                    tbsonuc.Text += "\r\n" + "Yeni Değerler şöyle olur:" + "\r\n" + "w1= " + yeniW1.ToString() + "\r\n"+ "w2= " + yeniW2.ToString() + "\r\n";
+                    return -1;
+                }
+            }
+            else
+            {
+                tbsonuc.Text += "\r\n"+ "Net <= ɸ olduğundan Ç" + ornekNo.ToString() + "= 0 olacaktır.";
+                if (beklenen == 0)
+                {
+                    tbsonuc.Text += "\r\n" + "Ç" + ornekNo.ToString() + "=B" + ornekNo.ToString() + " olduğundan ağırlıklar değişmez." + "\r\n";
+                    tbsonuc.Text += "W1 ve W2 değerli aynı kalır.\r\nw1=" + w1.ToString() + " w2=" + w2.ToString() + "\r\n";
+                    return 1;
+                }
+                else
+                {            
+                       yeniW1 = w1 + (ogrenme * x1);
+                    tbsonuc.Text += "\r\n" + "W1Yeni= " + w1.ToString() + "+(" + ogrenme.ToString() + "*" + x1.ToString() + ")";
+                    yeniW2 = w2 + (ogrenme * x2);
+                    tbsonuc.Text += "\r\n" + "W2Yeni= " + w2.ToString() + "+(" + ogrenme.ToString() + "*" + x2.ToString() + ")" + "\r\n";
+
+                    tbsonuc.Text += "\r\n" + "Yeni Değerler şöyle olur:" + "\r\n" + "w1= " + yeniW1.ToString() + "\r\n"+ "w2= " + yeniW2.ToString() + "\r\n";
+                    return -1;
+                    
+                }
+            }
+        }
+       
+    }
+}
